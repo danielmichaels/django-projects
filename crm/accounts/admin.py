5011afa28ce078be0afa25713e9574fa0c3e40ac
danielmichaels <dans.address@outlook.com>
2020-08-23 21:46:35 +1000

    Create models

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Customer, Product, Order
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,1 +1,51 @@
-# Create your models here.
+from django.db import models
+
+
+class Customer(models.Model):
+    # Have to register models in the admin.py file as well
+    name = models.CharField(max_length=255, null=True)
+    phone = models.CharField(max_length=255, null=True)
+    email = models.EmailField(max_length=255, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # in admin, if we don't declare this we just get a Customer object
+        return self.name
+
+
+class Product(models.Model):
+    """
+    Product item many-to-many
+    """
+
+    CATEGORY = (
+        ("Indoor", "Indoor"),
+        ("Outdoor", "Outdoor"),
+    )
+    name = models.CharField(max_length=255, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=255, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=255, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    """
+
+    """
+
+    STATUS = (
+        ("Pending", "Pending"),
+        ("Out for delivery", "Out for deliver"),
+        ("Delivered", "Delivered"),
+    )
+    # customer
+    # product
+    status = models.CharField(max_length=255, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status
