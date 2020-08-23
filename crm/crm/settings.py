7c4a29790190c505ee609ca2c534824746e1730c
danielmichaels <dans.address@outlook.com>
2020-08-23 20:54:48 +1000

    Created static and added custom css/images

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -115,3 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    # the magic of Pathlib
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL = "/images/"
