0f199c98910e469cddb6947d0463b0bee0f9f4b1
danielmichaels <dans.address@outlook.com>
2020-08-23 22:48:54 +1000

    Demonstrate how to return objects by filter and count into html.

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 
+from .models import Product, Order, Customer
+
 
 def home(request):
-    return render(request, template_name="accounts/dashboard.html")
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    pending_orders = orders.filter(status='Pending').count()
+    delivered_orders = orders.filter(status='Delivered').count()
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total_orders': total_orders,
+        'total_customers': total_customers,
+        'pending_orders': pending_orders,
+        'delivered_orders': delivered_orders,
+    }
+    return render(request, context=context,
+                  template_name="accounts/dashboard.html")
 
 
 def products(request):
-    return render(request, template_name="accounts/products.html")
+    products = Product.objects.all()
+    return render(request, template_name="accounts/products.html", context={
+        'products': products,
+    })
 
 
 def customers(request):
