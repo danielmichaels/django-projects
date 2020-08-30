5e67436251f5916bd4352aa25a515d160ee6ab3a
danielmichaels <dans.address@outlook.com>
2020-08-30 22:54:20 +1000

    Create templates and route them with bootstrap and custom css

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # internal apps
-    "blog_content",
+    "blog_content.apps.BlogContentConfig",
     # third party apps
 ]
 
--- blog/blog_content/views.py
+++ blog/blog_content/views.py
@@ -1,7 +1,22 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+posts = [
+    {'author': 'Roger Ramjet',
+     'title': 'blog post 1',
+     'content': 'lorem lorem',
+     'date_posted': '30 August 2020'},
+
+    {'author': 'Proton Pill',
+     'title': 'blog post 2',
+     'content': 'lorem lorem',
+     'date_posted': '31 August 2020'},
+]
 
 def home(request):
-    return HttpResponse('blog-home')
+    context = {"title": "blogZzZ", 'posts': posts}
+    return render(request, "blog_content/home.html", context)
+
 
 def about(request):
-    return HttpResponse("<marquee>helllloooo about</marquee>")
+    context = {}
+    return render(request, "blog_content/about.html", context)
