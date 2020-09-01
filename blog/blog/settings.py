68a05e4342d43fc3b0e520f2d60162d5fe24f969
danielmichaels <dans.address@outlook.com>
2020-09-01 21:57:25 +1000

    Login, Logout, and is_auth nav completed
    override auth_views with custom templates

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -120,3 +120,5 @@ STATIC_URL = "/static/"
 
 # Crispy Forms
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "blog-home"
--- blog/users/views.py
+++ blog/users/views.py
@@ -13,8 +13,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Sign Up Confirmed, Welcome {username}")
-            return redirect("blog-home")
+            messages.success(request, f"Sign Up Confirmed, Welcome {username}, Please Login")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", context={"form": form})
