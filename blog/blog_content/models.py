5a53ce9631dcd13937046a3e16743afbe88c3535
danielmichaels <dans.address@outlook.com>
2020-09-04 20:56:29 +1000

    CBV's and templates added

--- blog/blog_content/models.py
+++ blog/blog_content/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -12,6 +13,13 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        """
+        In CBV, the redirect chain on a form submit needs this, else
+        it can't redirect.
+        """
+        return reverse('post_detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title
--- blog/blog_content/views.py
+++ blog/blog_content/views.py
@@ -1,6 +1,98 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
 from blog_content.models import Post
+
+
+class PostListView(ListView):
+    """
+    CBV PostList
+
+    CBV templates: <app>/<model>_<viewtype>.html | 'blog_content/post_list.html
+    """
+
+    model = Post
+    template_name = "blog_content/home.html"  # else 'post_list.html'
+    context_object_name = "posts"  # else it looks for 'object_list' instead of post
+    ordering = ["-date_posted"]
+
+
+class PostDetailView(DetailView):
+    """
+    Each posts detailed view
+    """
+
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    """
+    User can write a post in browser
+    """
+
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        """
+        override the CBV to include author.id
+        """
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    User can update a post in browser
+    """
+
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        """
+        override the CBV to include author.id
+        """
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        """
+        Must declare this if using UserPassesTestMixin
+
+        Here we make sure only author can update their posts.
+        """
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """
+    Each posts detailed view
+    """
+
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        """
+        Must declare this if using UserPassesTestMixin
+
+        Here we make sure only author can update their posts.
+        """
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def home(request):
