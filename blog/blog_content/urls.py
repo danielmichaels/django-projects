f4a58cbec28f7638c695533201c297a7ae2af99f
danielmichaels <dans.address@outlook.com>
2020-09-05 22:14:22 +1000

    Add password resets

--- blog/blog_content/urls.py
+++ blog/blog_content/urls.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from blog_content import views
+from blog_content import views as user_views
 from .views import (
     PostListView,
     PostDetailView,
@@ -17,5 +18,29 @@ urlpatterns = [
     path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
-    path("about/", views.about, name="blog-about"),
+    path("about/", user_views.about, name="blog-about"),
+]
+
+# Password Reset etc
+urlpatterns += [
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+        name="password_reset_complete",
+    ),
 ]
