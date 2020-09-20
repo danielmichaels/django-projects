6306c38bab2edfa6b9baafa18c70efc472a88273
danielmichaels <dans.address@outlook.com>
2020-09-20 14:57:11 +1000

    Add basic test suite

--- polls/polls/views.py
+++ polls/polls/views.py
@@ -1,34 +1,51 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
 
 from .models import Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context=context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """
+        return last 5 questions
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", context={"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        exclude polls that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected = question.choice_set.get(pk=request.POST['choice'])
+        selected = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Question.DoesNotExist):
-        return render(request, 'polls/index.html',
-                      {'question': question,
-                       'error_message': "you didn't select a choice",})
+        return render(
+            request,
+            "polls/index.html",
+            {"question": question, "error_message": "you didn't select a choice",},
+        )
     else:
         selected.votes += 1
         selected.save()
