eb19d69ee14f01e147a3030028828dea684eef2e
danielmichaels <dans.address@outlook.com>
2020-10-07 11:11:46 +1100

    async chat rooms created

--- websockets/chats/consumers.py
+++ websockets/chats/consumers.py
@@ -1,16 +1,41 @@
 import json
+
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        # here we can do auth before accepting the connection
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+
+        # join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
+        # always call this last to ensure we've done all checks
         self.accept()
 
     def disconnect(self, code):
-        pass
+        # leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
+    # rx msg from websocket
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat_message", "message": message}
+        )
+
+    # rx msg from room group
+    def chat_message(self, event):
+        message = event["message"]
+
+        # send msg to websocket
         self.send(text_data=json.dumps({"message": message}))
